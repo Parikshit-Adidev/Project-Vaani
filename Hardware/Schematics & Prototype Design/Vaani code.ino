83528675e6d4bdef168712932ce38b9c2f075584
Parikshitsinh Jadeja <jadejaparikshit@outlook.com>
2025-11-30 10:18:31 +0530

    Add files via upload

---
+++ Hardware/Schematics & Prototype Design/Vaani code.ino
@@ -0,0 +1,112 @@
+#include <Vaani_inferencing.h>
+#include <driver/i2s.h>
+
+#define I2S_WS   15   
+#define I2S_SD   32   
+#define I2S_SCK  14   
+#define I2S_PORT I2S_NUM_0
+
+#define SAMPLE_BUFFER EI_CLASSIFIER_RAW_SAMPLE_COUNT
+static int32_t samples[SAMPLE_BUFFER];
+
+
+static signed short inferenceBuffer[SAMPLE_BUFFER];
+static bool debug_nn = false;
+
+
+void setupI2S() {
+    const i2s_config_t config = {
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+        .sample_rate = EI_CLASSIFIER_FREQUENCY, 
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+        .communication_format = I2S_COMM_FORMAT_I2S,
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+        .dma_buf_count = 8,
+        .dma_buf_len = 64,
+        .use_apll = false,
+        .tx_desc_auto_clear = false,
+        .fixed_mclk = 0
+    };
+
+    const i2s_pin_config_t pin_config = {
+        .bck_io_num = I2S_SCK,
+        .ws_io_num = I2S_WS,
+        .data_out_num = I2S_PIN_NO_CHANGE,
+        .data_in_num = I2S_SD
+    };
+
+    i2s_driver_install(I2S_PORT, &config, 0, NULL);
+    i2s_set_pin(I2S_PORT, &pin_config);
+    i2s_zero_dma_buffer(I2S_PORT);
+}
+
+void setup() {
+    Serial.begin(115200);
+    delay(1000);
+
+    Serial.println("\n Vaani + INMP441 Voice Classification");
+    Serial.println("Model: Parkinsons / Healthy / Background");
+    
+    setupI2S();
+}
+
+
+bool getAudioSamples() {
+    size_t bytesRead;
+    if (i2s_read(I2S_PORT, samples, sizeof(samples), &bytesRead, portMAX_DELAY) != ESP_OK) {
+        return false;
+    }
+    int count = bytesRead / 4;
+
+    for (int i = 0; i < count; i++) {
+        inferenceBuffer[i] = samples[i] >> 14; 
+    }
+    return true;
+}
+
+void loop() {
+
+    if (!getAudioSamples()) return;
+
+    
+    long sum = 0;
+    for (int i = 0; i < SAMPLE_BUFFER; i++)
+        sum += abs(inferenceBuffer[i]);
+
+    int volume = sum / SAMPLE_BUFFER;
+    Serial.print("Volume: ");
+    Serial.print(volume);
+    Serial.print(" | ");
+
+    
+    signal_t signal;
+    signal.total_length = SAMPLE_BUFFER;
+    signal.get_data = [](size_t offset, size_t length, float *out_ptr) {
+        numpy::int16_to_float(&inferenceBuffer[offset], out_ptr, length);
+        return 0;
+    };
+
+    ei_impulse_result_t result;
+    EI_IMPULSE_ERROR err = run_classifier(&signal, &result, debug_nn);
+    if (err != EI_IMPULSE_OK) {
+        Serial.println("Classifier error!");
+        return;
+    }
+
+    
+    Serial.print("Prediction → ");
+    float best = 0;
+    const char* label = "";
+
+    for (size_t i = 0; i < EI_CLASSIFIER_LABEL_COUNT; i++) {
+        if (result.classification[i].value > best) {
+            best = result.classification[i].value;
+            label = result.classification[i].label;
+        }
+    }
+
+    Serial.print(label);
+    Serial.print(" (");
+    Serial.print(best * 100.0f, 2);
+    Serial.println(" %
